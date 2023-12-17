1f647e3c1ef62603dba10f61f783b019f3b6dfc8
hmutaz <hudzaifahmutaz13@gmail.com>
2023-12-18 00:36:29 +0700

    Created result activity and layout - Added footer decoration to screens

---
+++ app/src/main/java/com/malletsplay/eyecare/ui/ResultActivity.kt
@@ -0,0 +1,12 @@
+package com.malletsplay.eyecare.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.malletsplay.eyecare.R
+
+class ResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+    }
+}
